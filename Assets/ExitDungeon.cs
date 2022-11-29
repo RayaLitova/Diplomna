6cbe24a81b055ff87323598756ffffb7de3dfde1
Raya Litova <48449090+RayaLitova@users.noreply.github.com>
2022-11-29 11:51:57 +0200

    Added dungeon exit

--- Assets/ExitDungeon.cs
+++ Assets/ExitDungeon.cs
@@ -5,15 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ExitDungeon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene("CityScene");
     }
 }
