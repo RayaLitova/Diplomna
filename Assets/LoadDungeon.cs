f8ad3c7561b5b87cc92880a87d72831ac073f212
Raya Litova <raya.litova23@gmail.com>
2022-12-04 00:48:49 +0200

    Added dungeon randomization

---
+++ Assets/LoadDungeon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadDungeon : MonoBehaviour
+{
+    [SerializeField] private int dungeon_count;
+
+    private void Awake()
+    {
+        Instantiate((GameObject)Resources.Load("DungeonPrefabs/Dungeon_" + Random.Range(1, dungeon_count + 1), typeof(GameObject)), Vector3.zero, Quaternion.identity);
+    }
+}
