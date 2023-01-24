99f17ca6369a1f3627470c6b0d4ef27d728d306f
Raya Litova <raya.litova23@gmail.com>
2023-01-25 01:49:54 +0200

    Added build

--- Assets/Scripts/CameraHideObjects.cs
+++ Assets/Scripts/CameraHideObjects.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class CameraHideObjects : MonoBehaviour
 {
--- Assets/Scripts/UI/FillSkillMenu.cs
+++ Assets/Scripts/UI/FillSkillMenu.cs
@@ -6,13 +6,13 @@ public class FillSkillMenu : MonoBehaviour
 {
     void Start()
     {
-        DirectoryInfo dir = new DirectoryInfo("Assets/resources/Skill_prefabs/UI");
+        DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/StreamingAssets/UI");
         FileInfo[] info = dir.GetFiles("*.prefab");
         int childNum = 0;
         foreach (FileInfo f in info)
         {
             Transform skillSlot = transform.GetChild(childNum);
-            GameObject skill = (GameObject)Instantiate(Resources.Load("Skill_prefabs/UI/" + f.Name.Remove(f.Name.Length - 7)) /*Remove "Clone"*/, skillSlot);
+            GameObject skill = (GameObject)Instantiate(Resources.Load("Skill_prefabs/UI/" + f.Name.Remove(f.Name.Length - 7)) /*Remove ".prefab"*/, skillSlot);
             skill.transform.SetSiblingIndex(1);
             skill.GetComponent<RectTransform>().anchoredPosition = new Vector3(4.5635e-07f, -4.5635e-07f, 0f); //skill slot position
             skill.GetComponent<RectTransform>().sizeDelta = new Vector2(0.41084f, 0.41084f);
