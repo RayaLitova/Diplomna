b97cea0d03e59dd50a2564875e193ec76b1286b2
Raya Litova <raya.litova23@gmail.com>
2023-02-14 21:19:04 +0200

    Added tutorial texts

--- Assets/ChangeTutorialText.cs
+++ Assets/ChangeTutorialText.cs
@@ -8,21 +8,42 @@ public class ChangeTutorialText : MonoBehaviour
     public static bool TavernTutorialPassed = false;
 
     private string[] tutorialTextsDungeon = new string[] { 
-        "Text1",
-        "Text2",
-        "Text3"
+        "Use WASD to move around",
+        "You can also use Shift to dash in a certain direction",
+        "Look out to not exit the safe zone, marked by this big blue wall",
+        "Before you exit you should select your abilities",
+        "Press K to open the skill menu",
+        "Hold Alt to show your cursor",
+        "Now you can drag and drop abilities into the left part of your action bar",
+        "After you've placed them they can be executed with QER",
+        "Be careful with your choices, you cannot change them once you've exited the safe zone",
+        "You can also use special items",
+        "Press I to open your inventory",
+        "There you'll see all magic items",
+        "You can only place the ones that you've obtained in you item bar (upper left corner)",
+        "During your trial in the dungeon, enemies will drop more items",
+        "This will be indicated with a blue diamond on top of your character's head",
+        "The items are reseted every time you enter the dungeon",
+        "The red circle in the middle of your action bar is your health",
+        "Once it's emptied your trial ends and you're sent back to the city",
+        "To pass the dungeon level you have to conquer all normal enemies in your path",
+        "Also you have to beat the final boss which will open the portal back to the city",
+        "The path to the boss was shown in the starting cutscene",
+        "Good luck!"
     };
 
     private string[] tutorialTextsCity = new string[] {
-        "t",
-        "t",
-        "t"
+        "This is the main city",
+        "Here all the citizens are your friends",
+        "You can walk around and see the village",
+        "There is one particular house that you're allowed to enter - The tavern",
+        "It's located right behind the well on the main square"
     };
 
     private string[] tutorialTextsTavern = new string[] {
-        "a",
-        "a",
-        "a"
+        "This is the tavern",
+        "Here you can listen to the cat's beautiful voice",
+        "Also you can talk to the bartender which will send you to you next dungeon"
     };
     private int current = 0;
     private Text textRef;
